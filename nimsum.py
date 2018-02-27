faeff1d2e956704ba1c17373812fbfbac6f3e170
goldfish <fishcaneatpeople@gmail.com>
2018-02-27 00:24:09 -0800

    process piles
    haven't decided how I want to iterate through the binary strings, left
    to right or right to left

--- nimsum.py
+++ nimsum.py
@@ -1,8 +1,9 @@
 # nimsum.py
 # just starting with calculating nimsum
 
-# binary addition "without carry"
-# num1 and num2 are each either 0 or 1
+
+# let ++ be binary addition "without carry".
+# num1 and num2 are each either 0 or 1.
 # 0 ++ 0 = 1 ++ 1 = 0
 # 0 ++ 1 = 1 
 # TODO add error check for not 0 or 1
@@ -13,14 +14,26 @@ def binaryADD_nocarry(num1, num2):
     return 1
   else:
     return 0
-  
 
-# takes two integers, returns nimsum
+# takes two non-negative integers, returns nimsum.
+# we cannot just convert to binary and add because 
+  # nimsum is binary addition "without carry".
 def nimsum(pile1, pile2):
-	
-	return
-	
+  # convert each to binary string, currently fills to width 8
+  # TODO pad with 0s for monster numbers
+  pile1_bin = format(pile1, '08b')
+  pile2_bin = format(pile2, '08b')
+  # currently assumes both binary strings are same length
+  # instead, should iterate from right to left and
+    # when one string ends, just fill the rest with zeros
+  
+  return None
+
+
 def testies():
   print(binaryADD_nocarry(1,1))
   print(binaryADD_nocarry(1,0))
   print(binaryADD_nocarry(0,0))
+  
+
+testies()
